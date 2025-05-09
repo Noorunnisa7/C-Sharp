3b52626a22e6e9b2f7581b5317a216e89c8bdd2a
noorunnisa7 <noorunnisa560@gmail.com>
2025-05-09 18:12:30 +0500

    Console write line, Data types and Variables

---
+++ Class1/firstApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World"); // Print 
+            Console.WriteLine("C# First Application");
+
+            //Console.WriteLine("Enter Your Name: ");
+            //Console.ReadLine(); // Get User data
+
+            //Console.WriteLine("Enter your Age");
+            //Console.ReadLine();
+
+            int number = 60; // integer 
+            string name = "Nisa"; // string 
+            float fnum = 12.21F; // float
+            double db = 12.12345;
+            char b = 'A';
+            bool tf = true;
+            Console.WriteLine($"Integer: {number}, String: {name}, Float {fnum}");
+
+
+            Console.ReadKey();
+        }
+    }
+}
